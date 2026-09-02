890fad8ffa45f5d906de893e89f21463b77b5566
hariprasad2512 <hprasad887@gmail.com>
2026-09-02 20:04:53 +0530

    Add Index HTML along with CSS

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ in "__main__":
+    app.run(debug=True)
